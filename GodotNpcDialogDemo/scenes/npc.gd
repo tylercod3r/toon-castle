55a6afcf9597a19ccd76c811d9d7921c30f3307f
tyler parker <tyler.parker@lan-party.com>
2024-08-01 18:40:53 -0600

    basic npc animation

--- GodotNpcDialogDemo/scenes/npc.gd
+++ GodotNpcDialogDemo/scenes/npc.gd
@@ -1,46 +1,47 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
+#region VARIABLES
+const SPEED = 2.0
 const JUMP_VELOCITY = 4.5
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var playback:AnimationNodeStateMachinePlayback = animation_tree["parameters/playback"]
+@export var point_of_interest:Node3D
 
 var player
 var provoked := false
 var aggro_range := 2.0 
+#endregion
 
+#region FUNCTIONS - BUILT-IN
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
-
-
-
-#func _process(_delta:float) -> void:
-	#if provoked:
-		#navigation_agent_3d.target_position = player.global_position
-
+	playback.travel("Walking0")
+	
+func _process(_delta:float) -> void:
+	if provoked:
+		# navigation_agent_3d.target_position = player.global_position
+		navigation_agent_3d.target_position = point_of_interest.global_position
+		playback.travel("Walking0")
+#endregion
+	
+#region FUNCTIONS - CUSTOM
+func enableWalkState()->void:
+	navigation_agent_3d.target_position = player.global_position
+	playback.travel("Walking0")
 
 func _physics_process(delta: float) -> void:
-	var next_position = navigation_agent_3d.target_position
-	
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	# Get the input direction and handle the movement/deceleration.
-	var direction = global_position.direction_to(next_position) 
-	
 	# #######################################################################
 	var distance = global_position.distance_to(player.global_position)
 	if distance <= aggro_range:
 		provoked = true
 	# #######################################################################
-	
-	
-	
-	
-	
+
+	var next_position = navigation_agent_3d.target_position
+	look_at(next_position)
+
+	var direction = global_position.direction_to(next_position) 
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -49,3 +50,4 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+#endregion
