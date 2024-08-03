4021c821af68854d6f377542b5f71119facade2f
tyler parker <tyler.parker@lan-party.com>
2024-08-03 03:32:04 -0600

    increased npc awareness range + cleanup

--- GodotNpcDialogDemo/scripts/npc.gd
+++ GodotNpcDialogDemo/scripts/npc.gd
@@ -1,6 +1,6 @@
 extends CharacterBody3D
 
-#region VARIABLES
+#region VARIABLE
 const SPEED = 1.3
 const POST_WAVE_DELAY = 10.0
 
@@ -9,67 +9,39 @@ const ANIM_WALKING := "Walking0"
 const ANIM_WAVING := "Waving0"
 
 @onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
-
-
-
-
 @onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var playback:AnimationNodeStateMachinePlayback = animation_tree["parameters/playback"]
+
 @export var points_of_interest:Array[Node3D] = []
 
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 var walking := false
-var awareness_range := 2.0 
+var awareness_range := 3.0 
 var post_wave_delay_temp := POST_WAVE_DELAY
 var player
 var player_noticed := false
 #endregion
 
-#region FUNCTIONS - NATIVE
+#region FUNCTION - NATIVE
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
-	navigation_agent_3d.target_position = points_of_interest[randi() % points_of_interest.size()].global_position
-	# enableWalkState()
+	
+	enableWanderState()
 	
 #func _process(delta:float) -> void:
 	#if walking:
-		#enableWalkState()
+		#enableWanderState()
 	#elif player_noticed:
 		## navigation_agent_3d.target_position = player.global_position
 		## navigation_agent_3d.target_position = point_of_interest.global_position
 		#post_wave_delay_temp -= delta
 		#
 		## if post_wave_delay_temp == POST_WAVE_DELAY:
-		#enableWaveState()
+		#enableSocializeState()
 	#else:
 		#enableIdleState()
 
-#func _physics_process(delta: float) -> void:
-	## Add the gravity.
-	#if not is_on_floor():
-		#velocity.y -= gravity * delta
-	#
-	## #######################################################################
-	#var distance = global_position.distance_to(player.global_position)
-	#if distance <= awareness_range:
-		#player_noticed = true
-		## look_at(player.global_position)
-	## #######################################################################
-#
-	#var next_position = navigation_agent_3d.target_position
-	## look_at(next_position)
-#
-	#var direction = global_position.direction_to(next_position) 
-	#if direction:
-		#velocity.x = direction.x * SPEED
-		#velocity.z = direction.z * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-		#velocity.z = move_toward(velocity.z, 0, SPEED)
-#
-	#move_and_slide()
-	
 func _physics_process(delta:float) -> void:
 	var current_location = global_transform.origin
 	var next_location = navigation_agent_3d.get_next_path_position()
@@ -78,48 +50,43 @@ func _physics_process(delta:float) -> void:
 	velocity = velocity.move_toward(new_velocity, 0.25)
 	
 	## #######################################################################
-	#var distance = global_position.distance_to(player.global_position)
-	#if distance <= awareness_range:
-		#player_noticed = true
-		## look_at(player.global_position)
-	look_at(next_location)
+	var distance = global_position.distance_to(player.global_position)
+	if distance <= awareness_range:
+		if not player_noticed:
+			enableSocializeState()
+	
+	if walking:
+		look_at(next_location)
+		playback.travel(ANIM_WALKING)
+	elif player_noticed:
+		look_at(player.global_position)
+		playback.travel(ANIM_IDLE)
 	## #######################################################################
 	
-	playback.travel(ANIM_WALKING)
-	
 	move_and_slide()
-	
-func update_target_location(target_location) -> void:
-	navigation_agent_3d.set_target_location(target_location)
 #endregion
 	
-#region FUNCTIONS - CUSTOM
-func enableIdleState()->void:
-	walking = false
-	player_noticed = false
-	
-	# navigation_agent_3d.target_position = player.global_position
-	navigation_agent_3d.target_position = mesh_instance_3d.global_position
-	playback.travel(ANIM_IDLE)
+#region FUNCTION - CUSTOM
+func set_target(target_position:Vector3) -> void:
+	navigation_agent_3d.target_position = target_position
 
-func enableWalkState()->void:
+func get_random_point_of_interest()->Vector3:
+	return points_of_interest[randi() % points_of_interest.size()].global_position
+
+func enableWanderState()->void:
 	walking = true
 	player_noticed = false
 	
-	# navigation_agent_3d.target_position = player.global_position
-	navigation_agent_3d.target_position = points_of_interest[randi() % points_of_interest.size()].global_position
-	playback.travel(ANIM_WALKING)
+	set_target(get_random_point_of_interest())
 	
-func enableWaveState()->void:
+func enableSocializeState()->void:
 	walking = false
 	player_noticed = true
 	
-	# navigation_agent_3d.target_position = player.global_position
-	playback.travel(ANIM_WAVING)
-	
-	post_wave_delay_temp = POST_WAVE_DELAY
+	set_target(global_position)
 #endregion
 
-
+#region FUNCTION - SIGNAL
 func _on_navigation_agent_3d_target_reached() -> void:
 	print("target reached!")
+#endregion
