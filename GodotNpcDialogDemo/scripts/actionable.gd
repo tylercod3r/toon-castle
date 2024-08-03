c2062bdb354e0877435438d3d7de484f1d2a7996
tyler parker <tyler.parker@lan-party.com>
2024-08-02 21:59:36 -0600

    dialog ui polish

--- GodotNpcDialogDemo/scripts/actionable.gd
+++ GodotNpcDialogDemo/scripts/actionable.gd
@@ -1,7 +1,12 @@
 extends Area3D
 
+const Balloon = preload("res://dialog/balloon.tscn")
+
 @export var dialogue_resource:DialogueResource
 @export var dialogue_start:String = "start"
+@onready var portrait: TextureRect = %Portrait
 
 func action() -> void:
-	DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialogue_start)
+	var balloon:Node = Balloon.instantiate()
+	get_tree().current_scene.add_child(balloon)
+	balloon.start(dialogue_resource, dialogue_start)
